70445ec0a2ad12fd7071b8df2e0dab5a1b7f3bfe
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-04-26 11:13:18 -0400

    pulled from develop

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,31 +1,35 @@
 const knex = require("knex")(require("../knexfile"));
 
 exports.index = (_req, res) => {
-  knex('warehouses')
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((err) => res.status(400).send(`Error retrieving Warehouses ${err}`));
+    knex("warehouses")
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch((err) =>
+            res.status(400).send(`Error retrieving Warehouses ${err}`)
+        );
 };
 
 exports.singleWarehouse = (req, res) => {
-    knex('warehouses')
-      .where({ id: req.params.id })
-      .then((data) => {
-        // If record is not found, respond with 404
-        if (!data.length) {
-          return res.status(404).send(`Record with id: ${req.params.id} is not found`);
-        }
-  
-        // Knex returns an array of records, so we need to send response with a single object only
-        res.status(200).json(data[0]);
-      })
-      .catch((err) =>
-        res.status(400).send(`Error retrieving warehouse ${req.params.id} ${err}`)
-      );
-  };
+    knex("warehouses")
+        .where({ id: req.params.id })
+        .then((data) => {
+            // If record is not found, respond with 404
+            if (!data.length) {
+                return res
+                    .status(404)
+                    .send(`Record with id: ${req.params.id} is not found`);
+            }
 
-
+            // Knex returns an array of records, so we need to send response with a single object only
+            res.status(200).json(data[0]);
+        })
+        .catch((err) =>
+            res
+                .status(400)
+                .send(`Error retrieving warehouse ${req.params.id} ${err}`)
+        );
+};
 
 exports.warehouseInventories = (req, res) => {
     knex("inventories")
@@ -44,4 +48,3 @@ exports.warehouseInventories = (req, res) => {
             res.status(500).json({ error: "Internal server error " });
         });
 };
-
--- routes/warehouseRoute.js
+++ routes/warehouseRoute.js
@@ -1,17 +1,10 @@
 const router = require("express").Router();
 const warehouseController = require("../controllers/warehouseController");
 
-router
-.route("/")
-.get(warehouseController.index);
+router.route("/").get(warehouseController.index);
 
 router.route("/:id/inventories").get(warehouseController.warehouseInventories);
 
-  router
-  .route('/:id')
-  .get(warehouseController.singleWarehouse)
-
-
+router.route("/:id").get(warehouseController.singleWarehouse);
 
 module.exports = router;
-
