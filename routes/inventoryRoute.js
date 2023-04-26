e6913d8da3ff97135e1b4154cb4807b7f46ad9c6
Gennesis Baez <gennesis.baez1@gmail.com>
2023-04-26 16:08:26 -0400

    final changes to POST/CREATE new inventory items

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const inventoryController = require('../controllers/inventoryController');
 
 router
-.route('/').get(inventoryController.index);
+    .route('/')
+    .get(inventoryController.index)
+    .post(inventoryController.addInventory);
 
 router
-.route('/:id')
-.get(inventoryController.singleInventory)
-.put(inventoryController.updateInventories);
+    .route('/:id')
+    .get(inventoryController.singleInventory)
+    .put(inventoryController.updateInventories);
 
 module.exports = router;
