0ad23b34007bc21058cf242cba3a800bc00bcfcb
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-04-26 11:24:20 -0400

    creating the put for inventories

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -9,3 +9,23 @@ exports.index = (_req, res) => {
       res.status(400).send(`Error retrieving Inventories: ${err}`)
     );
 };
+
+exports.updateInventories = (req, res) => {
+  knex("warehouse")
+      .where({ id: req.params.id })
+      .then((data) => {
+          // If record is not found, respond with 404
+          if (!data.length) {
+              return res
+                  .status(404)
+                  .send(`Record with id: ${req.params.id} is not found`);
+          }
+          // Knex returns an array of records, so we need to send response with a single object only
+          res.status(200).json(data[0]);
+      })
+      .catch((err) =>
+          res
+              .status(400)
+              .send(`Error retrieving warehouse ${req.params.id} ${err}`)
+      );
+};
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const inventoryController = require('../controllers/inventoryController');
 
-router.route('/').get(inventoryController.index);
+router
+.route('/').get(inventoryController.index);
+
+router
+.put('/:id')(inventoryController.updateInventories);
 
 module.exports = router;
