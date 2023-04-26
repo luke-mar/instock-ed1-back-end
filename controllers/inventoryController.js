21084ba29462368bd33f4b9a0e45937dcd7a055f
Lucas <lukemar112@gmail.com>
2023-04-26 11:31:43 -0400

    showing single inventory now working

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -9,3 +9,21 @@ exports.index = (_req, res) => {
       res.status(400).send(`Error retrieving Inventories: ${err}`)
     );
 };
+
+let emptyObject = {};
+exports.singleInventory = (req, res) => {
+  knex('inventories')
+    .where({ id: req.params.id })
+    .then((data) => {
+      // If record is not found, respond with 404
+      if (!data.length) {
+        return res.status(404).send(emptyObject);
+      }
+
+      // Knex returns an array of records, so we need to send response with a single object only
+      res.status(200).json(data[0]);
+    })
+    .catch((err) =>
+      res.status(400).send(`Error retrieving warehouse ${req.params.id} ${err}`)
+    );
+};
